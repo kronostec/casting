bee9d15cd0486fba98ebca494b5b30b1718bd7be
Jefferson Alves Cordeiro <jef_alves_c@hotmail.com>
2015-04-11 13:59:38 -0300

    Alterações na Controller

---
+++ projeto/casting/src/main/java/br/com/kronos/casting/controller/HomeController.java
@@ -0,0 +1,19 @@
+package br.com.kronos.casting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.kronos.commons.controller.KronosBaseController;
+
+@Controller
+public class HomeController extends KronosBaseController {
+
+	@RequestMapping("/*")
+	public String index(Model model){
+		System.out.println("teste");
+		
+		return "public/index";
+	}
+	
+}
