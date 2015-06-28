52ea972bf93d10430a1e07a0d00150f9f2500d23
Jefferson <jef_alves_c@hotmail.com>
2015-06-27 23:20:03 -0300

    Restrutoração do Facets

---
+++ projeto/casting/src/main/java/br/com/kronos/casting/domain/enumeration/TipoDocumento.java
@@ -0,0 +1,8 @@
+package br.com.kronos.casting.domain.enumeration;
+
+
+public enum TipoDocumento {
+
+	RG, CPF, CERTIDAO_NASCIMENTO, CERTIDA_CASAMENTO, PASSAPORTE, VISTO;
+	
+}
---
+++ projeto/casting/src/test/java/br/com/kronos/casting/controller/HomeControllerTest.java
@@ -0,0 +1,37 @@
+package br.com.kronos.casting.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class HomeControllerTest {
+	
+	
+
+	@Test
+	public void testIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContato() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFooter() {
+		fail("Not yet implemented");
+	}
+
+}
